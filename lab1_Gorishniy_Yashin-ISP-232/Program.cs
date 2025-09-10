2a208590107b0ac8e2082519c06cbd322e66d976
cokes238 <cokes238@gmail.com>
2025-09-10 13:40:54 +0300

    Добавьте файлы проекта.

---
+++ lab1_Gorishniy_Yashin-ISP-232/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace lab1_Gorishniy_Yashin_ISP_232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello world!");
+            //int n = 10;
+            //Console.WriteLine(n);
+            //Console.Write("имя:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Введите имя :{name}");
+            //Console.Write("возвраст:");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Введите возраст :{age}");
+
+            //int number = 250;
+            //long number2 = (byte)(number + 10);
+            //Console.WriteLine(number2);
+
+            //bool alive = true;
+            //bool isdead = false;
+
+            //char symbol = '2';
+
+            //double yNumber = 3.4;
+            //float inumber = 3.4f;
+            //decimal kNumber = 3.4m;
+
+            //object lNumber = 3.4;
+
+            //var gNumber = 10;
+
+            //самостоятельные задания 1 лаб
+
+            //1 
+            string z1 = "Форсаж";
+            int z2 = 3;
+            double z3 = Math.PI;
+            char z4 = 'G';
+
+            Console.WriteLine($"Любимое кино: {z1}");
+            Console.WriteLine($"Любимая цифра: {z2}");
+            Console.WriteLine($"Число Пи: {z3:F6}");
+            Console.WriteLine($"Любимая буква: {z4}");
+
+            //2
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+
+            //3
+            Console.WriteLine("Hello There");
+
+            //4
+            int m = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int ms = int.Parse(Console.ReadLine());
+
+            int total = (m + s + k + ms) * 3;
+
+            Console.WriteLine(total);
+
+            //5
+            Console.WriteLine("a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("b:");
+            double b = double.Parse(Console.ReadLine());
+            double c = 3 * Math.Pow(a + b, 3) + Math.Pow(275 * b, 2) - 127 * a - 41;
+            Console.WriteLine(c);
+
+            //6
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Привет, {name}!");
+            Console.WriteLine($"Сейчас тебе {age} лет.");
+            Console.WriteLine($"В следующем году тебе будет {age + 1}.");
+
+            //7
+
+        }
+    }
